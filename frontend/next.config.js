95cc726aa9a6ec906d4e75b24ade9cd490a58885
leroytan <mingxiang130@gmail.com>
2025-04-11 11:34:34 +0800

    Update backend link

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,22 +5,24 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination:
-          process.env.NODE_ENV === "development" || process.env.NODE_ENV === "production"
-            ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`
-            : "/api/",
+          process.env.NODE_ENV === "development"
+            ? `${process.env.NEXT_PUBLIC_API_BASE_URL_DEVELOPMENT}/:path*`
+            : process.env.NODE_ENV === "production"
+            ? `${process.env.NEXT_PUBLIC_API_BASE_URL_PRODUCTION}/:path*`
+            : `/api/:path*`,
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/py/docs`
+            ? `${process.env.NEXT_PUBLIC_API_BASE_URL_DEVELOPMENT}/py/docs`
             : "/api/py/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/py/openapi.json`
+            ? `${process.env.NEXT_PUBLIC_API_BASE_URL_DEVELOPMENT}/py/openapi.json`
             : "/api/py/openapi.json",
       },
     ];
