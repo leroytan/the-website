d5dbdd83f4c4fb35f96311febe40711032362d7b
chongtzezhao <47619363+chongtzezhao@users.noreply.github.com>
2025-05-12 03:11:15 +0800

    add stripe integration

--- backend/api/router/payment.py
+++ backend/api/router/payment.py
@@ -1,35 +1,32 @@
 import stripe
-from fastapi import APIRouter, Depends, HTTPException
+from api.config import settings
+from api.router.auth_utils import RouterAuthUtils
+from api.storage.models import User
+from fastapi import APIRouter, Depends, HTTPException, Request
 
-from backend.api.router.auth import RouterAuthUtils
+stripe.api_key = settings.stripe_api_key
 
 router = APIRouter()
 
-@router.post("/api/payment/checkout")
-def payment():
-    """
-    Create a checkout session with Stripe.
-    """
+@router.post("/api/payment/create-checkout-session")    
+async def create_checkout_session(request: Request, user: User = Depends(RouterAuthUtils.get_current_user)):
+    data = await request.json()
     try:
-        # Create a new checkout session
-        session = stripe.checkout.Session.create(
-            payment_method_types=["card"],
-            line_items=[
-                {
-                    "price_data": {
-                        "currency": "usd",
-                        "product_data": {
-                            "name": "T-shirt",
-                        },
-                        "unit_amount": 2000,
-                    },
-                    "quantity": 1,
-                },
-            ],
-            mode="payment",
-            success_url="https://example.com/success",
-            cancel_url="https://example.com/cancel",
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[{
+                'price': data['price_id'],  # Price ID from the Stripe dashboard
+                'quantity': 1,
+            }],
+            success_url=f"{data['success_url']}?session_id={{CHECKOUT_SESSION_ID}}",
+            cancel_url=data['cancel_url'],
+            metadata={
+                'app_email': user.email,  # Custom metadata to link Stripe data to your user
+            }
         )
-        return {"sessionId": session.id}
+        return {"session_id": checkout_session['id']}
+    except stripe.error.StripeError as e:
+        # Handle Stripe-specific errors
+        raise HTTPException(status_code=400, detail=str(e.user_message))
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
+    
