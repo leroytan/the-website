1dc14a6787ab0c93ba6d329dd6a034e5130027c0
chongtzezhao <47619363+chongtzezhao@users.noreply.github.com>
2025-05-29 17:41:14 +0800

    fix frontend html import

--- frontend/app/[...slug]/layout.tsx
+++ frontend/app/[...slug]/layout.tsx
@@ -6,11 +6,4 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return <>{children}</>
-  return (
-    <html lang="en">
-      <body>
-        {children}
-        </body>
-    </html>
-  )
 }
