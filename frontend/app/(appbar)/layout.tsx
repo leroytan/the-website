49464309953dce914b66d526c21a30c92eafb4cd
leroytan <mingxiang130@gmail.com>
2025-05-30 21:05:10 +0800

    Tutor onboarding page Redirect users using cookie if they intend to be tutor (middleware) Update useAuth to include user and tutor fields.

--- frontend/app/(appbar)/layout.tsx
+++ frontend/app/(appbar)/layout.tsx
@@ -1,6 +1,4 @@
 import ComponentLayout from "@/components/layout";
-import { BASE_URL } from "@/utils/constants";
-import { cookies } from "next/headers";
 
 export const dynamic = "force-dynamic";
 
@@ -9,32 +7,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("access_token")?.value;
-  let userWithRole = null;
 
-  if (accessToken) {
-    try {
-      const res = await fetch(`${BASE_URL}/me`, {
-        headers: {
-          Cookie: accessToken ? `access_token=${accessToken}` : "",
-        },
-        cache: "no-store",
-      });
-
-      if (res.ok) {
-        const data = await res.json();
-        const isTutor = data.tutor !== null;
-        userWithRole = {
-          ...data.user,
-          role: isTutor ? "tutor" : "client",
-          tutorProfile: data.tutor ?? null,
-        };
-      }
-    } catch (e) {
-      console.error("Error fetching user info:", e);
-    }
-  }
-
-  return <ComponentLayout user={userWithRole}>{children}</ComponentLayout>;
+  return <ComponentLayout>{children}</ComponentLayout>;
 }
--- frontend/app/(appbar)/logout/page.tsx
+++ frontend/app/(appbar)/logout/page.tsx
@@ -1,22 +1,24 @@
 'use client'
  
+import { useAuth } from '@/context/authContext'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
  
 export default function Logout() {
 
   const router = useRouter()
-
+  const { logout: logoutAuth } = useAuth();
   useEffect(() => {
     logout()
   })
 
   const logout = async () => {
+    
     const response = await fetch('/api/auth/logout', { method: 'POST' })
     if (!response.ok) {
     }
-    const data = await response.json()
-    console.log(data)
+    await logoutAuth()
+    
     router.push('/')
     router.refresh()
   }
