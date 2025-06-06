30d094110c8c3fa17344d46c3fbb4d496c4c04ca
chongtzezhao <47619363+chongtzezhao@users.noreply.github.com>
2025-06-07 01:07:11 +0800

    Switch NEXT_ENV_VERCEL to DEPLOY_TYPE, deprecate NODE_ENV

--- frontend/utils/constants.tsx
+++ frontend/utils/constants.tsx
@@ -1,7 +1,7 @@
-export const BASE_URL =
-process.env.NODE_ENV == null ||
-process.env.NODE_ENV === "development"
-? `${process.env.NEXT_PUBLIC_API_BASE_URL_DEVELOPMENT}`
-: process.env.NODE_ENV === "production"
-? `${process.env.NEXT_PUBLIC_API_BASE_URL_PRODUCTION}`
-: `/api/`
+export const BASE_URL = (process.env.DEPLOY_TYPE == 'local'
+  ? process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL
+  : process.env.DEPLOY_TYPE == 'development'
+  ? process.env.NEXT_PUBLIC_API_BASE_URL_DEVELOPMENT
+  : process.env.DEPLOY_TYPE == 'test'
+  ? process.env.NEXT_PUBLIC_API_BASE_URL_TEST
+  : process.env.NEXT_PUBLIC_API_BASE_URL_PRODUCTION) ?? "/api"; // Fallback to local if no match
