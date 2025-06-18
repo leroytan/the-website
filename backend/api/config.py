ad78bd494da59e07ce287702b27d7855105599db
Chong Tze Zhao <47619363+chongtzezhao@users.noreply.github.com>
2025-06-18 12:54:16 +0800

    Modularize environment configuration

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,10 +1,25 @@
-import datetime
+import os
+
+ENV = os.getenv("APP_ENV")
+
+print(f"Current environment: {ENV}")
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 # Load environment variables from .env file
-load_dotenv(".env")
+match ENV:
+    case "local":
+        load_dotenv(".env.local")
+    case "development":
+        load_dotenv(".env.development")
+    case "test":
+        load_dotenv(".env.test")
+    case "production":
+        load_dotenv(".env.production")
+    case _:
+        raise ValueError(f"Unknown environment: {ENV}. Please set APP_ENV to 'local', 'development', 'test', or 'production'.")
+    
 
 # Define the settings model
 class Settings(BaseSettings):
@@ -13,12 +28,8 @@ class Settings(BaseSettings):
     refresh_token_secret_key: str
     access_token_expire_minutes: int = 30  # Token expiry time
     refresh_token_expire_minutes: int = 60 * 24 * 7  # 7 days
-    database_config: str = "LOCAL"  # Default to using local DB
-    database_url_local: str
-    database_url_dev1: str
-    database_url_test: str = ""
-    database_url_prod: str
-    is_use_mock: bool = True
+    database_url: str
+    is_use_mock: bool = False
     db_populate_check: bool = False
     stripe_api_key: str
     stripe_product_name: str
@@ -32,28 +43,21 @@ class Settings(BaseSettings):
     gmail_credentials_json: str  # JSON string for Gmail credentials
     gmail_startup_notification_email: str = ""
 
-    # Dynamically determine which database URL to use
     @property
-    def database_url(self) -> str:
-        print("Using database config:", self.database_config)
-        match self.database_config:
-            case "LOCAL":
-                return self.database_url_local
-            case "DEV1":
-                return self.database_url_dev1
-            case "TEST":
-                return self.database_url_test
-            case "PROD":
-                return self.database_url_prod
+    def env(self):
+        return ENV
+    
+    @property
+    def is_database_local(self):
+        return self.database_url.split("@")[-1].startswith("localhost")
             
     def make_engine(self, create_engine: callable, NullPool: callable):
-        db_url = self.database_url
-        if db_url == "":
+        if self.database_url == "":
             raise ValueError("Database URL is not set. Please check your configuration.")
-        if self.database_config == "LOCAL":
-            return create_engine(db_url)
+        if self.is_database_local:
+            return create_engine(self.database_url)
         else:
-            return create_engine(db_url, client_encoding='utf8', poolclass=NullPool)
+            return create_engine(self.database_url, client_encoding='utf8', poolclass=NullPool)
 
 # Instantiate the settings object
 settings = Settings()
